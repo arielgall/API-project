3d9da0e8d3e857bf0d93bf0bb60cfd64b8a9d1a9
Ariel Gall <arielgall16@gmail.com>
2019-04-28 23:58:00 -0700

    Code from Pen
    input code from codepen pen

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 
+//Feed
+const getStuff = 'https://raw.githubusercontent.com/janaipakos/ghibliapi/master/data.json';
+
+let doStuff = function(data) {
+  let title, description;
+  
+  //console.log(data.films);
+  for (i=0; i < data.films.length; i++){
+    const template = `<h1>${data.films[i].title}</h1>
+<p>${data.films[i].description}</p>`;
+    $('#app').append(template);
+  }
+};
+
+$.getJSON(getStuff, doStuff);
+
+//let showStuff = function(data) {}
